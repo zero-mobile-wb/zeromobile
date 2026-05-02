7bd894cc074cc75c4ba23cd32a86fc2e5938e6e5
joshua <joshuasunday300@gmail.com>
2026-05-02 14:55:45 +0100

    chore: fixed Uncaught TypeError and Refrence Error

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
   optimizeDeps: {
     include: [
       '@privy-io/react-auth',
